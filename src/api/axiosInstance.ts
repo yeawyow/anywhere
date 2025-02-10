f8e04abbed43995b8a5d3699956e81e247d91ff9
yeawyow <yeawyow@gmail.com>
2025-02-10 20:12:02 +0700

    romove persit

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_URL } from "../config/constants";
+const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
--- src/jsvectormap.d.ts
+++ src/jsvectormap.d.ts
@@ -2,3 +2,10 @@ declare module 'jsvectormap' {
   const jsVectorMap: any;
   export default jsVectorMap;
 }
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
